b2348c8212cf0f288c8088cc94bc3e3412eba077
Miguel Barreto <miguelbarretosanz@gmail.com>
2018-03-21 10:54:42 +0100

    refactoring Visualization_sunburst

--- QS-AD/Visualization_sunburst.py
+++ QS-AD/Visualization_sunburst.py
@@ -15,9 +15,10 @@ from bokeh.plotting import figure
 
 
 #Import modules for interactive graphics
-from bokeh.layouts import column
-from bokeh.models import HoverTool
+from bokeh.layouts import column, widgetbox
+from bokeh.models import HoverTool, CustomJS, Slider
 from bokeh.io import curdoc
+from bokeh.models.widgets import Select
 
 #Import modules for conversions to radians.
 import math
@@ -28,7 +29,7 @@ import time
 #GETING DATA
 
 #Read data from Life Cycle format
-LC_data = pd.read_csv('LC_export 3.csv')
+LC_data = pd.read_csv('../data/Life Cycle/example/LC_export 3.csv')
 
 #END -- GETING DATA
 
@@ -46,7 +47,9 @@ p = figure(plot_width=width, plot_height=height, title="",
     x_range=(-420, 420), y_range=(-420, 420),
     min_border=0, outline_line_color="white",
     background_fill_color="#ffffff",
-    tools=[hover,])
+    tools=[hover])
+
+hover = HoverTool(tooltips = [("Name", "name")])
 
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
@@ -64,7 +67,7 @@ tr_inner_radius = fr_outer_radius+52+2,
 tr_outer_radius = fr_outer_radius+52+2+42
 
 def plot_ring(ring_number, start_time, duration, label, color, h_color):
-    "Create costumized annular_edges. start_time in HH:MM:SS"
+    "Create costumized annular_wedges. start_time in HH:MM:SS"
     if ring_number == 1:
         iner_radio = fr_inner_radius
         outer_radio = fr_outer_radius
@@ -76,8 +79,7 @@ def plot_ring(ring_number, start_time, duration, label, color, h_color):
         outer_radio = tr_outer_radius   
     else:
         print("ring_number value must be 1,2, or 3")  
-    
-    
+      
     #Convert HH:MM:SS format in radians 
     ts = time.strptime(start_time, "%H:%M:%S") 
     hour = (ts[3] + (ts[4]/60) + (ts[5]/3600))
@@ -91,19 +93,22 @@ def plot_ring(ring_number, start_time, duration, label, color, h_color):
     hour_rad_duration = math.radians(hour_duration * 15.0)
     duration_in_radians_to_plot = (hour_in_radians_to_plot + hour_rad_duration) 
     
-    #The annular wedge is plotted in the direccion "anticlock". I tried to use the diection "clock" (which seems more 
-    #logic for this plot) but I have some problems in the hit-testing. So I confirmed that it was a issue reported
+    #The annular wedge is plotted in the direccion "anticlock". I tried to use 
+    #the diection "clock" (which seems more 
+    #logic for this plot) but I had some problems in the hit-testing. 
+    #So I confirmed that it was a issue reported
     # with direction "clock : https://github.com/bokeh/bokeh/issues/2080
-     
+
     p.annular_wedge(
-        0, 0, iner_radio, outer_radio, hour_in_radians_to_plot - hour_rad_duration, duration_in_radians_to_plot - hour_rad_duration, 
+        0, 0, iner_radio, outer_radio, hour_in_radians_to_plot - hour_rad_duration, 
+        duration_in_radians_to_plot - hour_rad_duration, 
         color=color, hover_color=h_color , name="anular_wedges"
-        
     ) 
+    #Show the activity
+    return p
     
-    return 
 
-
+##example
 data1 = LC_data['START DATE(UTC)'][1]
 #Parse string into a naive datetime object.
 LCday,LCtime = data1.split(" ",1)
@@ -116,20 +121,24 @@ data3 = LC_data['START DATE(UTC)'][4]
 #Parse string into a naive datetime object.
 LCday3,LCtime3 = data3.split(" ",1)
 
-
 #First ring
-plot_ring(1,LCtime,LC_data[' DURATION'][1],"label","cyan","magenta")
+#plot_ring(1,LCtime,LC_data[' DURATION'][1],"label","cyan","magenta")
 #Second ring
-plot_ring(1,LCtime2,LC_data[' DURATION'][3],"label","red","pink")
+#plot_ring(1,LCtime2,LC_data[' DURATION'][3],"label","red","pink")
 #Third ring
-plot_ring(1,LCtime3,LC_data[' DURATION'][4],"label","green","grey")
+#plot_ring(1,LCtime3,LC_data[' DURATION'][4],"label","green","grey")
 
 #PLOT CLOCK
+
 clock_hours_to_plot = list(range(0,24))
 angles = 2*np.pi/24*pd.Series(list(range(0,24)))
-# radial axes
-p.annular_wedge(0, 0, fr_inner_radius, tr_outer_radius, angles, angles, color="lightgrey")
 
+def hours_axes():
+    # radial axes
+    p.annular_wedge(0, 0, fr_inner_radius, tr_outer_radius, angles, angles, 
+                    color="lightgrey")
+
+hours_axes()
 # hours labels
 labels = np.power(10.0, np.arange(-3, 4))
 minr = sqrt(log(.001 * 1E4))
@@ -142,9 +151,9 @@ yr = radii[0]*np.sin(np.array(angles))
 label_angle=np.array(angles)
 label_angle[label_angle < -np.pi/2] += np.pi # easier to read labels on the left side
 labels_24h_clock = list(range(6,-1,-1)) + list(range(23,6,-1))
-
 p.text(xr, yr, pd.Series(labels_24h_clock), angle=label_angle,
-       text_font_size="9pt", text_align="center", text_baseline="middle", text_color="lightgrey")
+       text_font_size="9pt", text_align="center", text_baseline="middle", 
+       text_color="lightgrey")
 
 
 #END -- PLOTING GRAPH
@@ -153,11 +162,44 @@ p.text(xr, yr, pd.Series(labels_24h_clock), angle=label_angle,
 
 
 #END -- CODE FOR THE INTEACTIVE PLOT
+#CODE FOR THE INTEACTIVE PLOT
 
-#SHOW ALL
-#show plot 
-#output_file("Visualization_sunburst.html", title="Visualization_sunburst.py example")
-#layout = column(p)
-#show(layout)
-curdoc().add_root(p)
+#slider to change the plot size
+callback = CustomJS(args=dict(xr=p.x_range),code="""
+    var a = -420;
+
+    //the model that triggered the callback is cb_obj:
+    var b = cb_obj.value;
+
+    //models passed as args are automagically available
+    xr.start = a;
+    xr.end = b;
+    
+""")
+
+# execute a callback whenever p.x_range.start changes
+p.x_range.js_on_change('start', callback)
+slider_x_range = Slider(start=10, end=1000, value=1, step=10, title="x_range", 
+                        callback=callback)
+
+
+
+def update_plot(attrname, old, new):
+    timestamp = select_timestamp.value
+    duration_index = (np.where(LC_data['START DATE(UTC)']== timestamp)[0])
+    LCday,LCtime = timestamp.split(" ",1)
+    #It can be improved by using a function that "clean"  the graphic 
+    #before ploting the new one.
+    plot_ring(1,LCtime,86399,"label","white","white")
+    plot_ring(1,LCtime,LC_data[" DURATION"][duration_index[0]],
+              LC_data[" NAME"][duration_index[0]],"red","blue")
+    hours_axes()
+    
+#Timestamp selection
+select_timestamp = Select(title="Datastamp", value="foo", 
+                          options=pd.Series.tolist(LC_data['START DATE(UTC)']))
+select_timestamp.on_change('value', update_plot)
+
+#SHOW ALL ON SERVER
+curdoc().add_root(column(slider_x_range, widgetbox(select_timestamp),p))
 
